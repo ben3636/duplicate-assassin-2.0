21c64e4a29b63a48cbc6f90775faa43996ef37d3
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2020-06-02 16:55:56 -0400

    Updated Process.bash

--- Process.bash
+++ Process.bash
@@ -1,17 +1,24 @@
 #!/bin/bash
+
+if [[ $1 == "" ]] || [[ $2 == "" ]]
+then
+	echo "Usage: ./Process.bash [results file] [target dir]"
+	exit 0
+fi
+
 ###---Review candidates---###
 mkdir -p review-folder
 mkdir -p process-logs
-cat RESULTS-SIZEDATE.txt | while read line
+cat $1 | while read line
 do
 	echo $line | tr ' ==%%==> ' '\n' > group.txt
 	cat group.txt | sed /^$/d | while read line
 	do
-		mv $1/$line review-folder	
+		mv $2/$line review-folder	
 	done
 	echo "Press Enter When Ready:"
 	echo
 	read input </dev/tty
-	mv review-folder/* $1
+	mv review-folder/* $2
 done
 mv group.txt process-logs
