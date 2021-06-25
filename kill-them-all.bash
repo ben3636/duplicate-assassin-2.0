517171c49de6097708228e2cee69b90e57903aec
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2021-06-25 10:57:22 -0400

    Update kill-them-all.bash

--- kill-them-all.bash
+++ kill-them-all.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-fuckers=$(ps -ef | grep ./scan | awk ' { print $2 } ')
+bois=$(ps -ef | grep ./scan | awk ' { print $2 } ')
 for i in {1..2}
 do
-	for i in $fuckers
+	for i in $bois
 	do
 		echo "Killing...$i"
 		kill $i
